c2750cc62ee682ce4f94a84ff669fa638bd3e5e1
Mick00 <20918417+Mick00@users.noreply.github.com>
2022-02-02 15:30:28 -0500

    add git import feature

--- src/ts2famix-cli.ts
+++ src/ts2famix-cli.ts
@@ -1,57 +1,77 @@
 import * as fs from "fs"
 import yargs, { Argv, exit } from "yargs";
 import { TS2Famix } from "./ts2famix";
+import git from "nodegit";
+
+const JSON_MODEL_DIRECTOY = './JSONModels/';
+const REPOS_DIRECTORY = './repos/';
+const LATEST_COMMIT = 'latest'
 
 const argv = yargs
     .example('$0 -i ../myTypescriptProject -o myTypeScriptProject.json', 'creates JSON-format model of typescript project')
+    .example('$0 -u https://github.com/microsoft/vscode.git -c fbdc009642ceffecb5f69a3bfdc78dd1124d64b5 -c 884273e35b35404e99e4787af0b026119e2a24dc', 'creates JSON-format model of typescript project')
     .alias('i', 'input')
-    .nargs('i', 1)
     .alias('o', 'output')
-    .nargs('o', 1)
-    .demandOption('input').demandOption('output').argv;
+    .alias('u', 'url')
+    .alias('c','commit')
+    .array('c')
+    .default({c: [LATEST_COMMIT]})
+    .argv;
 
-const importer = new TS2Famix();
-const paths = new Array<string>();
-paths.push(argv.input as string);
+const input = argv.input as string;
+const output = argv.output as string;
+const url = argv.url as string;
+let commits = argv.commit as string[];
 
-const fmxRep2 = importer.famixRepFromPath(paths);
-const jsonOutput = fmxRep2.getJSON();
-const fileName = argv.output as string;
-let directory = './JSONModels/' + fileName
-fs.writeFile(directory, jsonOutput, (err) => {
-    if (err) { throw err; }
-});
-console.info(`created ${fileName}`);
+let importation: Promise<string[]>;
+if (url){
+    importation = processFromGit(url, commits, [input]);
+} else{
+    importation = Promise.resolve([process([input], output)])
+}
+
+importation.then((outputs)=>{
+    console.info(`created ${outputs.join(", ")}`);
+})
 
 
-// import * as fs from "fs"
-// import yargs, { Argv, exit } from "yargs";
-// import { TS2Famix } from "./ts2famix";
+function process(inputs: string[], output: string) {
+    const importer = new TS2Famix();
+    const fmxRep2 = importer.famixRepFromPath(inputs);
+    const jsonOutput = fmxRep2.getJSON();
+    let directory = JSON_MODEL_DIRECTOY + output
+    fs.writeFile(directory, jsonOutput, (err) => {
+        if (err) { throw err; }
+    });
+    return directory
+}
 
-// class ts2famixCli {
-//     private argv = yargs
-//         .example('$0 -i ../myTypescriptProject -o myTypeScriptProject.json', 'creates JSON-format model of typescript project')
-//         .alias('i', 'input')
-//         .nargs('i', 1)
-//         .alias('o', 'output')
-//         .nargs('o', 1)
-//         .demandOption('input').demandOption('output').argv;
+async function processFromGit(link: string, commits: string[], inputs: string[]){
+    const projectName = getProjectName(link);
+    const projectDir = REPOS_DIRECTORY + projectName;
+    const repo = await cloneRepo(link, projectDir);
+    const outputs: string[] = [];
+    for(let commitSHA of commits){
+        if (commitSHA !== LATEST_COMMIT){
+            console.info("Checking out commit", commitSHA)
+            const commit = repo.getCommit(commitSHA);
+        }
+        const output = await process(inputs.map(input => `${projectDir}/${input}`), `${projectName}-${commitSHA}`);
+        outputs.push(output);
+    }
+    return outputs;
+}
 
-//     private importer = new TS2Famix();
-//     private paths = new Array<string>();
+function getProjectName(link: string){
+    //https://github.com/microsoft/vscode.git
+    return link.split("/").slice(-2).join("-").replace(".git", "");
+}
 
-//     public runImporter() {
-//         this.paths.push(this.argv.input as string);
-
-//         const fmxRep2 = this.importer.famixRepFromPath(this.paths);
-//         const jsonOutput = fmxRep2.getJSON();
-//         const fileName = this.argv.output as string;
-//         fs.writeFile(fileName, jsonOutput, (err) => {
-//             if (err) { throw err; }
-//         });
-//         console.info(`created ${fileName}`);
-//     }
-// }
-
-// let importer = new ts2famixCli();
-// importer.runImporter();
+async function cloneRepo(link: string, target: string){
+    if(fs.existsSync(target)){
+        console.log("Repo already exists");
+        return git.Repository.open(target);
+    }
+    console.log("Downloading repo");
+    return git.Clone(link, target)
+}
